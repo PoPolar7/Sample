4a827cef8192d395728ca637b669293e9012efd9
강태현 <0923kth@naver.com>
2025-03-04 18:19:27 +0900

    도서소개 페이지 완성

--- ch02/js/ui.js
+++ ch02/js/ui.js
@@ -32,4 +32,17 @@ $(document).ready(function () {
   $(".roll_right").click(function () {
     $(".book_roll li").eq(-1).insertBefore(".book_roll li:first-child");
   });
+  $(".book_roll li").click(function () {
+    var _this = $(this);
+    var liurl = _this.data("url");
+    $(".notebook").html();
+    $.ajax({
+      type: "GET",
+      url: liurl,
+      dataType: "html",
+      success: function (data) {
+        $(".notebook").html(data);
+      },
+    });
+  });
 });
